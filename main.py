cec422e3ec1bc5acd4e9c5d2160682659aae69f4
Guilherme Alencar <163006803+GuilhermeAlencar22@users.noreply.github.com>
2026-01-16 12:43:08 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,11 @@ import sys
 import json
 from ingest import run_ingestion
 from analytics import run_analytics
-
-
 def main():
     if len(sys.argv) < 2:
-        print("Uso:")
-        print("  python main.py ingest")
-        print("  python main.py analytics")
+        print("use:")
+        print("python main.py ingest")
+        print("python main.py analytics")
         return
 
     cmd = sys.argv[1].lower()
@@ -20,10 +18,8 @@ def main():
     elif cmd == "analytics":
         result = run_analytics()
         print(json.dumps(result, ensure_ascii=False, indent=2))
-
     else:
-        print("Comando inválido. Use: ingest ou analytics")
-
+        print("Comando errado")
 
 if __name__ == "__main__":
     main()
